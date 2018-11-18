b5b799ebec0777ff8d2aac1e282bb8b06d1a5189
Neuroma <jadrenifeni@gmail.com>
2018-11-18 22:59:46 +0000

    first commit,empty project

---
+++ Famous-Quotes/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatTabsModule  } from '@angular/material';
+import { AppComponent } from './app.component';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatTabsModule,
+    BrowserAnimationsModule
+    
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
